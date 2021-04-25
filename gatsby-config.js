00a4669c3358184844bb1d944ea6745d8cbe8c15
Sam Mc Nally <sam.mcnally94@gmail.com>
2021-04-25 19:03:02 +0100

    fix: title fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: "/rugby-moments",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rugby Moments`,
+    description: `Kick off your next Rugby Moment.`,
+    author: `@rugbymomentss`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
